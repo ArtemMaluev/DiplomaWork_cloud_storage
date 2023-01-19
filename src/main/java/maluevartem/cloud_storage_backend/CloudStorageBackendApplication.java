57d83241c920ca69046d23768782d96408e028a2
ArtemMaluev <artem.rus13@yandex.ru>
2023-01-19 21:29:33 +0300

    add logback.xml

--- src/main/java/maluevartem/cloud_storage_backend/CloudStorageBackendApplication.java
+++ src/main/java/maluevartem/cloud_storage_backend/CloudStorageBackendApplication.java
@@ -10,11 +10,11 @@ import java.util.GregorianCalendar;
 @Slf4j
 @SpringBootApplication
 public class CloudStorageBackendApplication {
+    static Calendar calendar = new GregorianCalendar();
 
     public static void main(String[] args) {
-        Calendar calendar = new GregorianCalendar();
         log.info("Сервер запущен: {}", calendar.getTime());
+
         SpringApplication.run(CloudStorageBackendApplication.class, args);
     }
-
 }
